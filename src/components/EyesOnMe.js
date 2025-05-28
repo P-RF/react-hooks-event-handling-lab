0c0fec0658bda8817862f09ee85faa507b1fe6cb
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-05-28 19:38:08 -0400

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  const HandleFocus = () => {console.log('Good!')};
+  const HandleBlur = () => {console.log('Hey! Eyes on me!')};
+
+  return (
+    <div>
+      <button onFocus={HandleFocus} onBlur={HandleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
